9acb60de0ff07ffaeeaf2674f66ea0f5a50acafb
countincognito <countincognito@outlook.com>
2015-07-04 18:23:11 +0100

    Excluding .nuspec file from git and updating assembly info.

--- Zametek.Utility/Properties/AssemblyInfo.cs
+++ Zametek.Utility/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Zametek.Utility")]
-[assembly: AssemblyDescription("Zametek.Utility")]
+[assembly: AssemblyDescription("Zametek utility classes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Riccardo M. Bennett-Lovsey")]
 [assembly: AssemblyProduct("Zametek.Utility")]
