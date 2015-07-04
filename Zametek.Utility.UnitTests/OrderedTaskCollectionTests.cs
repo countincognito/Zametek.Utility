52f9c10d5dc3fd558807089528b095f261efa19c
countincognito <countincognito@outlook.com>
2015-07-04 15:54:42 +0100

    OrderedTaskCollection added.

---
+++ Zametek.Utility.UnitTests/OrderedTaskCollectionTests.cs
@@ -0,0 +1,121 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zametek.Utility.UnitTests
+{
+    [TestFixture]
+    public class OrderedTaskCollectionTests
+    {
+        [Test]
+        public void OrderedTaskCollection_CtorWhenCalledWithTaskList_ShouldSucceed()
+        {
+            var tasks = new OrderedTaskCollection<int>(
+                new[]
+                {
+                    Task.FromResult(1),
+                });
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void OrderedTaskCollection_CtorWhenCalledWithNullTaskList_ShouldThrowArgumentNullException()
+        {
+            var tasks = new OrderedTaskCollection<int>((IEnumerable<Task<int>>)null);
+            Assert.Fail();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void OrderedTaskCollection_CtorWhenCalledWithNullFuncList_ShouldThrowArgumentNullException()
+        {
+            var tasks = new OrderedTaskCollection<int>((IEnumerable<Func<int>>)null);
+            Assert.Fail();
+        }
+
+        [Test]
+        public void OrderedTaskCollection_ForeachWhenCalledWithTimedTasks_ShouldReturnInCorrectOrder()
+        {
+            var task0 = Task.FromResult(0);
+            var task1 = Task.Run<int>(() =>
+            {
+                Thread.Sleep(100);
+                return 1;
+            });
+            var task2 = Task.Run<int>(() =>
+            {
+                Thread.Sleep(200);
+                return 2;
+            });
+            var tasks = new OrderedTaskCollection<int>(
+                new[]
+                {
+                    task1,
+                    task2,
+                    task0,
+                });
+            var results = new List<int>();
+
+            foreach (Task<int> task in tasks)
+            {
+                results.Add(task.Result);
+            }
+
+            Assert.That(results[0], Is.EqualTo(0));
+            Assert.That(results[1], Is.EqualTo(1));
+            Assert.That(results[2], Is.EqualTo(2));
+        }
+
+        [Test]
+        public void OrderedTaskCollection_ForeachWhenCalledWithTimedFuncs_ShouldReturnInCorrectOrder()
+        {
+            var tasks = new OrderedTaskCollection<int>(
+               new List<Func<int>>(new Func<int>[]
+            {
+               () =>
+               {
+                   Thread.Sleep(100);
+                   return 1;
+               },
+               () => 0, 
+               () =>
+               {
+                   Thread.Sleep(200);
+                   return 2;
+               },
+            }));
+            var results = new List<int>();
+
+            foreach (Task<int> task in tasks)
+            {
+                results.Add(task.Result);
+            }
+
+            Assert.That(results[0], Is.EqualTo(0));
+            Assert.That(results[1], Is.EqualTo(1));
+            Assert.That(results[2], Is.EqualTo(2));
+        }
+
+        [Test]
+        [ExpectedException(typeof(AggregateException))]
+        public void OrderedTaskCollection_ForeachFuncs_ShouldReturnInCorrectOrder()
+        {
+            var tasks = new OrderedTaskCollection<int>(
+               new List<Func<int>>(new Func<int>[]
+            {
+               () =>
+               {
+                   throw new Exception();
+               },
+            }));
+            var results = new List<int>();
+            foreach (Task<int> task in tasks)
+            {
+                results.Add(task.Result);
+            }
+            Assert.Fail();
+        }
+    }
+}
---
+++ Zametek.Utility/OrderedTaskCollection.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zametek.Utility
+{
+    public class OrderedTaskCollection<T>
+       : IEnumerable<Task<T>>
+    {
+        #region Fields
+
+        private object m_Lock = new object();
+        private Queue<TaskCompletionSource<T>> m_CompletionSources;
+        private IList<Task<T>> m_PendingTasks;
+        private Queue<Task<T>> m_CompletedTasks;
+
+        #endregion
+
+        #region Ctors
+
+        public OrderedTaskCollection(IEnumerable<Func<T>> lambdas)
+            : this(lambdas.Select(x => Task.Run(x)))
+        {
+        }
+
+        public OrderedTaskCollection(IEnumerable<Task<T>> tasks)
+        {
+            if (tasks == null)
+            {
+                throw new ArgumentNullException("tasks");
+            }
+            m_PendingTasks = new List<Task<T>>(tasks);
+            m_CompletedTasks = new Queue<Task<T>>();
+            m_CompletionSources = new Queue<TaskCompletionSource<T>>();
+            foreach (Task<T> task in m_PendingTasks)
+            {
+                task.ContinueWith(x =>
+                {
+                    lock (m_Lock)
+                    {
+                        m_PendingTasks.Remove(x);
+                        if (m_CompletionSources.Any())
+                        {
+                            SetResult(m_CompletionSources.Dequeue(), x);
+                        }
+                        else
+                        {
+                            m_CompletedTasks.Enqueue(x);
+                        }
+                    }
+                });
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        private bool HasTasks
+        {
+            get
+            {
+                lock (m_Lock)
+                {
+                    return m_PendingTasks.Any() || m_CompletedTasks.Any();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private Task<T> NextOrderedTask()
+        {
+            lock (m_Lock)
+            {
+                var tcs = new TaskCompletionSource<T>();
+                if (m_CompletedTasks.Any())
+                {
+                    Task<T> completedTask = m_CompletedTasks.Dequeue();
+                    SetResult(tcs, completedTask);
+                }
+                else
+                {
+                    m_CompletionSources.Enqueue(tcs);
+                }
+                return tcs.Task;
+            }
+        }
+
+        private static void SetResult(TaskCompletionSource<T> tcs, Task<T> task)
+        {
+            if (tcs == null)
+            {
+                throw new ArgumentNullException("tcs");
+            }
+            if (task == null)
+            {
+                throw new ArgumentNullException("task");
+            }
+            if (task.IsFaulted)
+            {
+                tcs.SetException(task.Exception);
+                return;
+            }
+            if (task.IsCanceled)
+            {
+                tcs.SetCanceled();
+                return;
+            }
+            tcs.SetResult(task.Result);
+        }
+
+        #endregion
+
+        #region IEnumerable<Task<T>>
+
+        public IEnumerator<Task<T>> GetEnumerator()
+        {
+            while (HasTasks)
+            {
+                yield return NextOrderedTask();
+            }
+        }
+
+        #endregion
+
+        #region IEnumerable
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
