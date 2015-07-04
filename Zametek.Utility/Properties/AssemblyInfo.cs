5754179657e626ca4f6568d03c830df3e0e4333d
countincognito <countincognito@outlook.com>
2015-07-05 00:47:52 +0100

    Updated project settings.

--- Zametek.Utility/Properties/AssemblyInfo.cs
+++ Zametek.Utility/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Riccardo M. Bennett-Lovsey")]
 [assembly: AssemblyProduct("Zametek.Utility")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
